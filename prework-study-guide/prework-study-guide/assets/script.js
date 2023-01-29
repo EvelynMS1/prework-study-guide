5d3f6bb442c5d3292a8fea5b7d1fba1242b2ce46
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-01-28 23:52:55 -0800

    Created a variable with an array for the contents along with a for loop to display multiple indeces

---
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -0,0 +1,20 @@
+var topics=["HTML", "CSS", "Git", "JavaScript"];
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+/*
+if(topic=== 'HTML'){
+    console.log("Lets study HTML!");
+} else if (topic === "CSS"){
+    console.log("Lets study CSS!");
+} else if (topic === "Git"){
+    console.log("Lets study Git!");
+}else if (topic === "Javascript"){
+    console.log("Let's study Javascript!" );
+} else{
+    console.log("Please try again!");
+}*/
+var shapes = ["traingle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
